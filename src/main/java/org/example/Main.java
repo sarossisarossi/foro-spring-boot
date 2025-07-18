0550adb034369ab7aad8e3b4d95274a53b870c0d
Sergio Rossi <sarossi@gmail.com>
2025-07-18 17:32:00 -0400

    segundo commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,23 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+@SpringBootApplication
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main { // Puedes renombrar esta clase a 'ForoApplication' o similar si lo deseas
+
+    public static void main(String[] args) {
+        // Este método arranca la aplicación Spring Boot.
+        // Aquí es donde Spring Boot lee application.properties,
+        // configura el datasource y ejecuta Flyway.
+        SpringApplication.run(Main.class, args);
+
+        // Los System.out.println que tenías son de una aplicación Java normal y no son necesarios aquí,
+        // a menos que quieras que se ejecuten después de que la aplicación Spring Boot haya arrancado.
+        // Para probar la base de datos, simplemente inicia la app y revisa tu BD.
+        System.out.println("La aplicación Spring Boot ha iniciado.");
     }
 }
